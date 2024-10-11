f052b73b2747de3bcdcc538d2bad846a28ea9e70
earlc <earlclyde197975@gmail.com>
2024-10-11 22:26:52 +0800

    lab 3 act 1 done

--- lab3_act1.py
+++ lab3_act1.py
@@ -1,1 +1,8 @@
-#start
+numbers = [2, 4, 3]
+
+for number in numbers:  # Use 'number' instead of 'numbers' to iterate over the list
+    if number % 3 == 0:  # Check if divisible by 3
+        number = number ** 2
+        print(f"{number} is divisible by 3")
+    else:
+        print(f"{number} is not divisible by 3")
