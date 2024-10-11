b20c0880c2e59c20cd423d978383cf251160c8f7
earlc <earlclyde197975@gmail.com>
2024-10-11 23:21:35 +0800

    lab 3 act 3 done

--- lab3_act3.py
+++ lab3_act3.py
@@ -1,1 +1,13 @@
-#start
+class Character:
+    vowels = ['a', 'e', 'i', 'o', 'u']
+
+    def __init__(self):
+        self.input_string = input("Input a string: ")
+        self.convert_vowels()
+
+    def convert_vowels(self):
+        result = ''.join([char.upper() if char in self.vowels else char for char in self.input_string])
+        print(result)
+
+# Create an instance of the class to run the program
+Character()
